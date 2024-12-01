ce95efdbcec4f8aca6eda64d1e9a33ff41392fbb
Ludvik Bjørklund <90049756+LudBjork@users.noreply.github.com>
2024-12-01 15:08:19 +0100

    Problem1 part2 (#2)

--- solutions/problem1.go
+++ solutions/problem1.go
@@ -10,16 +10,12 @@ import (
 )
 
 func main() {
-	input := ReadInput("./inputs/problem_1.txt")
-	// _, str because first element in range is the index
-	var left []int
-	var right []int
-	// See problem_1.txt for why SEPARATOR is like this
-	SEPARATOR := "   "
-	for _, str := range input {
-		left = append(left, SimpleStringToInt(strings.Split(str, SEPARATOR)[0]))
-		right = append(right, SimpleStringToInt(strings.Split(str, SEPARATOR)[1]))
-	}
+	left, right := ReadInput("./inputs/problem_1.txt")
+	//part1(left, right)
+	part2(left, right)
+}
+
+func part1(left []int, right []int) {
 
 	// sort both arrays in-place
 	sort.Slice(left, func(i, j int) bool {
@@ -29,6 +25,7 @@ func main() {
 	sort.Slice(right, func(i, j int) bool {
 		return right[i] < right[j]
 	})
+
 	var distances []int
 	for index, _ := range left {
 		distances = append(distances, ComputeDistance(left[index], right[index]))
@@ -38,6 +35,32 @@ func main() {
 		sum += partial
 	}
 	fmt.Println(sum)
+}
+
+func part2(left []int, right []int) {
+	similarityMap := CreateSimilarityMap(left, right)
+	sum := 0
+	for _, value := range left {
+		sum += value * similarityMap[value]
+	}
+	fmt.Println(sum)
+}
+
+func CreateSimilarityMap(left []int, right []int) map[int]int {
+	var similarityMap map[int]int
+	similarityMap = make(map[int]int)
+
+	for _, valueLeft := range left {
+		// check if already iterated to minimise repeats
+		if similarityMap[valueLeft] == 0 {
+			for _, valueRight := range right {
+				if valueLeft == valueRight {
+					similarityMap[valueLeft]++
+				}
+			}
+		}
+	}
+	return similarityMap
 }
 
 func ComputeDistance(left int, right int) int {
@@ -56,7 +79,7 @@ func SimpleStringToInt(in string) int {
 
 // Returns every line of input file as a slice.
 // ENSURE INPUT HAS NO BLANK LINE
-func ReadInput(filepath string) []string {
+func ReadInput(filepath string) ([]int, []int) {
 	file, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
@@ -69,5 +92,15 @@ func ReadInput(filepath string) []string {
 	}
 
 	file.Close()
-	return input
+
+	var left []int
+	var right []int
+	// See problem_1.txt for why SEPARATOR is like this
+	SEPARATOR := "   "
+	for _, str := range input {
+		left = append(left, SimpleStringToInt(strings.Split(str, SEPARATOR)[0]))
+		right = append(right, SimpleStringToInt(strings.Split(str, SEPARATOR)[1]))
+	}
+
+	return left, right
 }
