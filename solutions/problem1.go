975ac49464adb62f6c96f2fdb86cd0b3a9a54f21
LudBjork <ludvikb@hotmail.no>
2024-12-01 14:05:22 +0100

    Rewrite: Use scanner instead. Safely convert to int

--- solutions/problem1.go
+++ solutions/problem1.go
@@ -4,30 +4,52 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	input := ReadInput("../inputs/problem_1.txt")
+	input := ReadInput("./inputs/problem_1.txt")
 	// _, str because first element in range is the index
+	var left []int
+	var right []int
+	// See problem_1.txt for why SEPARATOR is like this
+	SEPARATOR := "   "
 	for _, str := range input {
-		fmt.Print(str)
+		left = append(left, UnpackStringConvertToInt(strings.Split(str, SEPARATOR)[0]))
+		right = append(right, UnpackStringConvertToInt(strings.Split(str, SEPARATOR)[1]))
 	}
+
+	sort.Slice(left, func(i, j int) bool {
+		return left[i] < left[j]
+	})
+
+	sort.Slice(right, func(i, j int) bool {
+		return right[i] < right[j]
+	})
+	fmt.Println(left)
 }
 
-// Returns every line of input string as an array.
-// Further finetuning is left to each method giving the solution
+func UnpackStringConvertToInt(in string) int {
+	out, err := strconv.Atoi(in)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
+
+// Returns every line of input file as a slice.
+// ENSURE INPUT HAS NO BLANK LINE
 func ReadInput(filepath string) []string {
 	file, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
 	}
-	reader := bufio.NewReader(file)
+	scanner := bufio.NewScanner(file)
 	var input []string
-	for {
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			break
-		}
+	for scanner.Scan() {
+		line := scanner.Text()
 		input = append(input, line)
 	}
 
