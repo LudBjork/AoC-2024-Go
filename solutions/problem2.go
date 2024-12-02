36cedf16a280a5175430ba009947b18bac125b2a
LudBjork <ludvikb@hotmail.no>
2024-12-02 19:29:12 +0100

    Solve part 1

--- solutions/problem2.go
+++ solutions/problem2.go
@@ -33,18 +33,29 @@ func p2_part1(input []string) {
 }
 
 func isReportSafe(report []int) bool {
+	var strictInc bool
+	var strictDec bool
 	for i, _ := range report {
 		if i > 0 {
+			if i == 1 {
+				strictInc = report[i] > report[i-1]
+				strictDec = report[i] < report[i-1]
+			}
+
+			strictInc = strictInc && (report[i] > report[i-1])
+			strictDec = strictDec && (report[i] < report[i-1])
+
 			if commons.ComputeDistance(report[i], report[i-1]) == 0 {
 				return false
 			}
 			if commons.ComputeDistance(report[i], report[i-1]) > 3 {
 				return false
 			}
-			if (report[i] > report[0]) != (report[i-1] > report[0]) {
-				return false
-			}
 		}
 	}
+	if !strictInc && !strictDec {
+		return false
+	}
+
 	return true
 }
